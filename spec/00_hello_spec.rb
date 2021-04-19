6e02af5ac1fdc144917e050201bc9184c997b760
lucaskantor <kantor.lucas@live.fr>
2021-04-19 17:34:53 +0200

    tests

--- spec/00_hello_spec.rb
+++ spec/00_hello_spec.rb
@@ -6,12 +6,3 @@ describe "the hello function" do
   end
 end
 
-describe "the greet function" do
-  it "says hello to someone" do
-    expect(greet("Alice")).to eq("Hello, Alice!")
-  end
-
-  it "says hello to someone else" do
-    expect(greet("Bob")).to eq("Hello, Bob!")
-  end
-end
--- spec/01_temperature_spec.rb
+++ spec/01_temperature_spec.rb
@@ -1,4 +1,4 @@
-require_relative '../lib/01_temperature'
+require_relative '../lib/01_temperature.rb'
 
 describe "temperature conversion functions" do
 
